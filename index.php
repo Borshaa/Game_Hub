b92ad378507356b1e714d0a25dcd905cf0a59b0b
Sadia Afrin <95537184+Borshaa@users.noreply.github.com>
2023-11-29 23:05:48 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,18 @@
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<style>
+
+h1{
+    text-align: justify;
+	font-size: 1.2em !important;
+}
+h2{
+    text-align: center;
+	font-size: 1.2em !important;
+}
+</style>
 </head>
 <body>
 <?php include 'Navbar.php' ?> 
@@ -38,14 +50,12 @@
 
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
-    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    	<p><h2>Welcome <strong><?php echo $_SESSION['username']; ?></strong></h2></p>
+		<p> <h1> We warmly welcome you to "Gamers' Hub", where gaming is not just a hobby, it's a way of life! We are thrilled to have you here and excited to introduce you to a world of endless possibilities in the gaming universe. Our mission is to provide all the gamers with all kind of gaming products. You can watch gameplay of different games and also download different newly developed games. If you are a new game developer, don't worry about marketing your game. You can reach out to us and the rest is on ours! So come on in, explore our platform, and let us help you unleash your true gaming potential! </h1>
+	</p>
+	
     <?php endif ?>
 </div>
-        <br>
-		</br>
-    <div class = "container">    
-		Welcome to "Gamers' Hub", where gaming is not just a hobby, it's a way of life! We are thrilled to have you here and excited to introduce you to a world of endless possibilities in the gaming universe. Our mission is to provide all the gamers with all kind of gaming products. You can watch gameplay of different games and also download different newly developed games. If you are a new game developer, don't worry about marketing your game. You can reach out to us and the rest is on ours! So come on in, explore our platform, and let us help you unleash your true gaming potential!
-    </div> 
+
     </body>
 </html>
