e64d293a6568962b700cc2ec8027e60178422c6a
Sadia Afrin <95537184+Borshaa@users.noreply.github.com>
2023-12-14 19:45:08 +0600

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,29 @@
+<?php include 'Navbar.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Logout</title>
+</head>
+
+<style>
+
+h1{
+    text-align: center;
+    font-size: 2em !important;
+}
+
+</style>
+
+
+<body>
+
+<div class="container">
+    <p class="desc"><b><h1>Are you sure you want to logout?</b></h1><br></p>
+    <h1> <a href="index.php?logout='1'" class="btn btn-primary">Yes</a>
+    <a href="index.php" class="btn btn-primary">No</a> </h1>
+
+
+</div>
+</body>
+</html>
